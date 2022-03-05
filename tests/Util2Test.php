99a02d22e4a31e0a059bf9300dada73b6b465c81
MrAnyx <1ptitigre@gmail.com>
2022-03-05 01:22:27 +0100

    Added tests

---
+++ tests/Util2Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class Util2Test extends TestCase
+{
+	public function testSomething(): void
+	{
+		$this->assertTrue(true);
+	}
+}
